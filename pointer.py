ef615bc1b1c50b5365ef3b527567d46fcebb26d3
ZWL <7330385+esvhd@users.noreply.github.com>
2018-05-16 00:44:30 +0100

    code reformat

--- pointer.py
+++ pointer.py
@@ -11,12 +11,13 @@ import model
 
 from utils import batchify, get_batch, repackage_hidden
 
-parser = argparse.ArgumentParser(description='PyTorch PennTreeBank RNN/LSTM Language Model')
+parser = argparse.ArgumentParser(
+    description='PyTorch PennTreeBank RNN/LSTM Language Model')
 parser.add_argument('--data', type=str, default='data/penn',
                     help='location of the data corpus')
 parser.add_argument('--model', type=str, default='LSTM',
                     help='type of recurrent net (LSTM, QRNN)')
-parser.add_argument('--save', type=str,default='best.pt',
+parser.add_argument('--save', type=str, default='best.pt',
                     help='model to use the pointer over')
 parser.add_argument('--cuda', action='store_false',
                     help='use CUDA')
@@ -49,16 +50,20 @@ test_data = batchify(corpus.test, test_batch_size, args)
 ntokens = len(corpus.dictionary)
 criterion = nn.CrossEntropyLoss()
 
+
 def one_hot(idx, size, cuda=True):
     a = np.zeros((1, size), np.float32)
     a[0][idx] = 1
     v = Variable(torch.from_numpy(a))
-    if cuda: v = v.cuda()
+    if cuda:
+        v = v.cuda()
     return v
+
 
 def evaluate(data_source, batch_size=10, window=args.window):
     # Turn on evaluation mode which disables dropout.
-    if args.model == 'QRNN': model.reset()
+    if args.model == 'QRNN':
+        model.reset()
     model.eval()
     total_loss = 0
     ntokens = len(corpus.dictionary)
@@ -66,18 +71,22 @@ def evaluate(data_source, batch_size=10, window=args.window):
     next_word_history = None
     pointer_history = None
     for i in range(0, data_source.size(0) - 1, args.bptt):
-        if i > 0: print(i, len(data_source), math.exp(total_loss / i))
+        if i > 0:
+            print(i, len(data_source), math.exp(total_loss / i))
         data, targets = get_batch(data_source, i, evaluation=True, args=args)
         output, hidden, rnn_outs, _ = model(data, hidden, return_h=True)
         rnn_out = rnn_outs[-1].squeeze()
         output_flat = output.view(-1, ntokens)
         ###
         # Fill pointer history
-        start_idx = len(next_word_history) if next_word_history is not None else 0
-        next_word_history = torch.cat([one_hot(t.data[0], ntokens) for t in targets]) if next_word_history is None else torch.cat([next_word_history, torch.cat([one_hot(t.data[0], ntokens) for t in targets])])
-        #print(next_word_history)
-        pointer_history = Variable(rnn_out.data) if pointer_history is None else torch.cat([pointer_history, Variable(rnn_out.data)], dim=0)
-        #print(pointer_history)
+        start_idx = len(
+            next_word_history) if next_word_history is not None else 0
+        next_word_history = torch.cat([one_hot(t.data[0], ntokens) for t in targets]) if next_word_history is None else torch.cat(
+            [next_word_history, torch.cat([one_hot(t.data[0], ntokens) for t in targets])])
+        # print(next_word_history)
+        pointer_history = Variable(rnn_out.data) if pointer_history is None else torch.cat(
+            [pointer_history, Variable(rnn_out.data)], dim=0)
+        # print(pointer_history)
         ###
         # Built-in cross entropy
         # total_loss += len(data) * criterion(output_flat, targets).data[0]
@@ -94,12 +103,16 @@ def evaluate(data_source, batch_size=10, window=args.window):
         for idx, vocab_loss in enumerate(softmax_output_flat):
             p = vocab_loss
             if start_idx + idx > window:
-                valid_next_word = next_word_history[start_idx + idx - window:start_idx + idx]
-                valid_pointer_history = pointer_history[start_idx + idx - window:start_idx + idx]
+                valid_next_word = next_word_history[
+                    start_idx + idx - window:start_idx + idx]
+                valid_pointer_history = pointer_history[
+                    start_idx + idx - window:start_idx + idx]
                 logits = torch.mv(valid_pointer_history, rnn_out[idx])
                 theta = args.theta
-                ptr_attn = torch.nn.functional.softmax(theta * logits).view(-1, 1)
-                ptr_dist = (ptr_attn.expand_as(valid_next_word) * valid_next_word).sum(0).squeeze()
+                ptr_attn = torch.nn.functional.softmax(
+                    theta * logits).view(-1, 1)
+                ptr_dist = (ptr_attn.expand_as(valid_next_word)
+                            * valid_next_word).sum(0).squeeze()
                 lambdah = args.lambdasm
                 p = lambdah * ptr_dist + (1 - lambdah) * vocab_loss
             ###
--- weight_drop.py
+++ weight_drop.py
@@ -2,7 +2,9 @@ import torch
 from torch.nn import Parameter
 from functools import wraps
 
+
 class WeightDrop(torch.nn.Module):
+
     def __init__(self, module, weights, dropout=0, variational=False):
         super(WeightDrop, self).__init__()
         self.module = module
@@ -13,18 +15,22 @@ class WeightDrop(torch.nn.Module):
 
     def widget_demagnetizer_y2k_edition(*args, **kwargs):
         # We need to replace flatten_parameters with a nothing function
-        # It must be a function rather than a lambda as otherwise pickling explodes
-        # We can't write boring code though, so ... WIDGET DEMAGNETIZER Y2K EDITION!
+        # It must be a function rather than a lambda as otherwise pickling
+        # explodes We can't write boring code though, so ...
+        # WIDGET DEMAGNETIZER Y2K EDITION!
         # (╯°□°）╯︵ ┻━┻
         return
 
     def _setup(self):
-        # Terrible temporary solution to an issue regarding compacting weights re: CUDNN RNN
+        # Terrible temporary solution to an issue regarding compacting weights
+        # re: CUDNN RNN
         if issubclass(type(self.module), torch.nn.RNNBase):
-            self.module.flatten_parameters = self.widget_demagnetizer_y2k_edition
+            self.module.flatten_parameters = \
+                self.widget_demagnetizer_y2k_edition
 
         for name_w in self.weights:
-            print('Applying weight drop of {} to {}'.format(self.dropout, name_w))
+            print('Applying weight drop of {} to {}'
+                  .format(self.dropout, name_w))
             w = getattr(self.module, name_w)
             del self.module._parameters[name_w]
             self.module.register_parameter(name_w + '_raw', Parameter(w.data))
@@ -35,20 +41,24 @@ class WeightDrop(torch.nn.Module):
             w = None
             if self.variational:
                 mask = torch.autograd.Variable(torch.ones(raw_w.size(0), 1))
-                if raw_w.is_cuda: mask = mask.cuda()
-                mask = torch.nn.functional.dropout(mask, p=self.dropout, training=True)
+                if raw_w.is_cuda:
+                    mask = mask.cuda()
+                mask = torch.nn.functional.dropout(
+                    mask, p=self.dropout, training=True)
                 w = mask.expand_as(raw_w) * raw_w
             else:
-                w = torch.nn.functional.dropout(raw_w, p=self.dropout, training=self.training)
+                w = torch.nn.functional.dropout(
+                    raw_w, p=self.dropout, training=self.training)
             setattr(self.module, name_w, w)
 
     def forward(self, *args):
         self._setweights()
         return self.module.forward(*args)
 
+
 if __name__ == '__main__':
     import torch
-    from weight_drop import WeightDrop
+    # from weight_drop import WeightDrop
 
     # Input is (seq, batch, input)
     x = torch.autograd.Variable(torch.randn(2, 1, 10)).cuda()
@@ -91,7 +101,8 @@ if __name__ == '__main__':
     print('Run 1:', run1)
     print('Run 2:', run2)
 
-    # First time step, not influenced by hidden to hidden weights, should be equal
+    # First time step, not influenced by hidden to hidden weights, should be
+    # equal
     assert run1[0] == run2[0]
     # Second step should not
     assert run1[1] != run2[1]
